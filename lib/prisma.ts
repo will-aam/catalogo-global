b1bb348001b0a4fa026cd6a1815cb7d31e087a4e
Will <will00987654@gmail.com>
2026-04-14 20:58:51 -0300

    feat(prisma): integrate Neon adapter and update Prisma configuration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+// Extraímos e forçamos a tipagem para garantir que não vá undefined
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// O erro ocorre aqui porque o Pool precisa da string EXATA
+const pool = new Pool({ connectionString: connectionString });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const adapter = new PrismaNeon(pool as any);
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query"],
   });
 
